33c8665544cb837fa2d3296ce5c3ea15a04f2122
sigurdsvela <sigurdbergsvela@gmail.com>
2015-10-05 12:00:03 +0200

    Include the wordpress test library in this repo

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,10 +6,7 @@ require_once __DIR__ . "/../vendor/sigurdsvela/std/autoloader.php";
 require_once __DIR__ . "/../core/autoload.php";
 require_once __DIR__ . "/WPRequireTestUtils.php";
 
-$_tests_dir = getenv( 'WP_TESTS_DIR' );
-if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
-}
+$_tests_dir = __DIR__ . '/wordpress-tests-lib';
 
 require_once $_tests_dir . '/includes/functions.php';
 
