b3f400274063a1ff13fa7ac604567b21ab0e1f21
sigurdsvela <sigurdbergsvela@gmail.com>
2015-10-05 15:13:47 +0200

    Move test-libs outside repo. Moving them in f'ed with travis stuff, and I understood why it's bad practice

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,7 +6,7 @@ require_once __DIR__ . "/../vendor/sigurdsvela/std/autoloader.php";
 require_once __DIR__ . "/../core/autoload.php";
 require_once __DIR__ . "/WPRequireTestUtils.php";
 
-$_tests_dir = __DIR__ . '/wordpress-tests-lib';
+$_tests_dir = '/tmp/wp-require-unittest/wordpress-test-library';
 
 require_once $_tests_dir . '/includes/functions.php';
 
