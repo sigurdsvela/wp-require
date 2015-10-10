86f0b97930b221c59f5818caa99c4583e91d36b1
sigurdsvela <sigurdbergsvela@gmail.com>
2015-10-10 02:13:21 +0200

    Bump to version 0.1.1

--- wp-require.php
+++ wp-require.php
@@ -3,7 +3,7 @@
  * Plugin Name: WP Require
  * Plugin URI:  https://github.com/sigurdsvela/wp-require
  * Description: A WordPress plugin to handle plugin dependencies
- * Version:     0.1
+ * Version:     0.1.1
  * Author:      Sigurd Svela
  * Author URI:  https://github.com/sigurdsvela
  * Text Domain: wp-require
